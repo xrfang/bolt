04f87b9c1008160b71b6dc52a79683ec079b002a
xrfang <xrfang@gmail.com>
2023-12-28 18:04:50 +0800

    WIP: startup routing

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"embed"
+	"fmt"
+	"os"
 
 	"github.com/wailsapp/wails/v2"
 	"github.com/wailsapp/wails/v2/pkg/options"
@@ -12,12 +14,9 @@ import (
 var assets embed.FS
 
 func main() {
-	// Create an instance of the app structure
 	app := NewApp()
-
-	// Create application with options
-	err := wails.Run(&options.App{
-		Title:  "bolt",
+	opts := options.App{
+		Title:  "BoltDB Client",
 		Width:  1024,
 		Height: 768,
 		AssetServer: &assetserver.Options{
@@ -25,12 +24,10 @@ func main() {
 		},
 		BackgroundColour: &options.RGBA{R: 27, G: 38, B: 54, A: 1},
 		OnStartup:        app.startup,
-		Bind: []interface{}{
-			app,
-		},
-	})
+		Bind:             []any{app},
+	}
 
-	if err != nil {
-		println("Error:", err.Error())
+	if err := wails.Run(&opts); err != nil {
+		fmt.Fprintln(os.Stderr, "ERROR:", err)
 	}
 }
